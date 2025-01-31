809bbb53131deee0602a8dbb9318f21c51187339
Bohdan Tron <bohdan.tron@gmail.com>
2025-01-31 15:47:15 +0200

    feat: add user signup with validation and service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // @InjectRepository(User) is used because of generic type Repository<User>
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+}
