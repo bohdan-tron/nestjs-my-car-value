9aef802a723e8677e594aa6612bf3b6585de19fc
Bohdan Tron <bohdan.tron@gmail.com>
2025-02-02 12:57:59 +0200

    style: add clearify comments

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,19 +8,22 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
 
+// dto interface to check if it is a class
 interface ClassConstructor {
   new (...args: any[]): {}
 }
 
+// Custom Decorator
 export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
+// Custom Interceptor
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
 
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    // runs before a request is handled
+    // runs before a request is handled, context is the request object
 
     return handler.handle().pipe(
       map((data: any) => {
