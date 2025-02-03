ec6e76fab4306d0144fec75cbb6e7ebff36223d2
Bohdan Tron <bohdan.tron@gmail.com>
2025-02-03 20:04:19 +0200

    feat: configured swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 const cookieSession = require('cookie-session');
 
@@ -15,6 +16,15 @@ async function bootstrap() {
       whitelist: true,
     })
   );
+  const config = new DocumentBuilder()
+    .setTitle('NestJS RESTful API')
+    .setDescription('NestJS RESTful API description')
+    .setVersion('1.0')
+    .addTag('NestJS RESTful API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
